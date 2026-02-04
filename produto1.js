385e32a40855f232e9bc70bf9ed80bc31b290698
Jair <danieljatoba034@hotmail.com>
2026-02-04 14:45:15 -0300

    Delete produto1.js

--- produto1.js
+++
@@ -1,7 +0,0 @@
-{
-  "nome": "SSD M.2 NVME",
-  "tamanho": "512GB",
-  "descricao": "Peça OEM (Usada) com saude acima de 80%",
-  "preco": 235.00,
-  "link": "https://cdn.awsli.com.br/600x700/2460/2460093/produto/372827243/ssd-nvme-m2-256gb-hana-electronics-oem-01-8i59ta889f.jpg"
-}
